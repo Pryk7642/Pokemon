2ed6a9cbb3fae008a449591e8534eed86bde3c16
Pryk7642 <110373747+Pryk7642@users.noreply.github.com>
2023-02-12 17:30:37 +0700

    Add files via upload

---
+++ Lab_pokemon/Lab_Pokemon_dex-main/Lab_Pokemon_dex/pokemons/Beedrill.cs
@@ -0,0 +1,23 @@
+﻿using Lab_Pokemon_dex.pokemons;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Pokemon_dex
+{
+    internal class Beedrill : Pokemon
+    {
+        public Beedrill() 
+        {
+            Random random = new Random();
+            this.name = "Steelix";
+            //random number between >= 430 and < 525
+            this.HP = random.Next(500, 1050);
+            this._image = Properties.Resources._020;
+            this.Attack = random.Next(166, 307);
+            this.Defense = random.Next(10,50);
+        }
+    }
+}
